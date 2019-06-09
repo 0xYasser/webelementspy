7b8ca3ac853ab1769cac69eefd8c48afbfbb4ea2
Yasser A <yalshalaan@gmail.com>
2019-06-09 16:58:34 -0400

    tests and fix for import from local

--- autoaid/webdriver.py
+++ autoaid/webdriver.py
@@ -1,61 +1,80 @@
+import sys
+import pathlib
 from selenium import webdriver
-import platform
-import pathlib as path
 
 _browsers = {
     **dict.fromkeys(['chrome','chromedriver'],'chromedriver'), 
     **dict.fromkeys(['firefox', 'ff', 'geckodriver'], 'geckodriver'),
     **dict.fromkeys(['safari'], 'safari')
     }
-_os_map = {'Linux':'linux', 'Darwin':'mac', 'Windows':'win'}
+_os_map = {'Linux':'linux', 'darwin':'mac', 'Windows':'win'}
 
 _MAC_SAFARI_DRIVER_PATH = '/usr/bin/safaridriver'
 
 
 
 class WebDriver():
-    def __init__(self, browser):
-        self.driver = self._web_driver(browser)
+    def __init__(self, browser, headless = False):
+        self.driver = self._web_driver(browser, headless)
 
-    def _web_driver(self, brw):
-        browser = self._identify_browser(brw.lower())
-        exe_path = self._find_executable_path(browser)
+    def _web_driver(self, brw, headless):
+        browser = self._identify_browser(brw)
+        os = self._identify_os()
+        exe_path = self._find_executable_path(browser, os)
         kwargs = {'executable_path':exe_path}
-        driver = None
-
+        
         if browser == 'chromedriver':
+            if headless:
+                options = webdriver.ChromeOptions()
+                options.headless = True
+                kwargs['options'] = options
             driver = self._chrome(**kwargs)
+
         elif browser == 'geckodriver':
+            if headless:
+                options = webdriver.FirefoxOptions()
+                options.headless = True
+                kwargs['options'] = options
             driver = self._firefox(**kwargs)
+
         elif browser == 'safari':
             driver = self._safari(**kwargs)
+
         return driver
 
     def _identify_browser(self, brw):
         try:
-            return _browsers[brw]
+            return _browsers[brw.lower()]
         except KeyError:
-            raise TypeError(f'not supported browser ({brw})')
-
-    def _find_executable_path(self, brw):
-        os = self._find_os()
-        if os == 'mac' and brw == 'safari':
-            return _MAC_SAFARI_DRIVER_PATH
-        else:
-            return path.PurePath('../drivers', os, brw)
-
-    def _find_os(self):
-        os_id = platform.system()
+            raise NotImplementedError(f'not supported browser ({brw})')
+    
+    def _identify_os(self, os = None):
+        os_id = sys.platform if not os else os
         try:
             return _os_map[os_id]
         except KeyError:
-            raise TypeError(f'not supported OS ({os_id})')
+            raise NotImplementedError(f'not supported OS ({os_id})')
+
+    def _find_executable_path(self, brw, os):
+        exe_path = None
+        if os == 'mac' and brw == 'safari':
+            exe_path = pathlib.PurePath(_MAC_SAFARI_DRIVER_PATH)
+        else:
+            abs_path = pathlib.Path(__file__).resolve().parents[1] 
+            drivers_path = abs_path / 'drivers'
+            exe_path = pathlib.PurePath(drivers_path / os / brw)
+
+        if pathlib.Path(exe_path).is_file():
+            return exe_path
+        else:
+            raise FileNotFoundError(f'cannot find driver at {exe_path}')
 
     def _chrome(self, **kwargs):
-        return webdriver.Chrome(**kwargs)
+        return webdriver.Chrome(**kwargs,)
 
     def _firefox(self, **kwargs):
         return webdriver.Firefox(**kwargs)
 
     def _safari(self, **kwargs):
         return webdriver.Safari(**kwargs)
+
---
+++ tests/test_webdriver.py
@@ -0,0 +1,66 @@
+import unittest, sys, pathlib
+from autoaid import webdriver as wd
+
+
+class WebDriverTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+    # def test__init__chrome(self):
+    #         wd.WebDriver('chrome', headless=True).driver.close()
+    
+    # def test__init__firefox(self):
+    #         wd.WebDriver('firefox', headless=True).driver.close()
+    
+    # @unittest.skipUnless(sys.platform.startswith("darwin"), "requires MacOS")
+    # def test__init__safari(self):
+    #         wd.WebDriver('safari').driver.close()
+
+    def test_identify_browser(self):
+        with self.assertRaises(NotImplementedError):
+            wd.WebDriver._identify_browser(self,'NetScape')
+
+        input_expected = {
+            **dict.fromkeys(['chrome','chromedriver'],'chromedriver'), 
+            **dict.fromkeys(['firefox', 'ff', 'geckodriver'], 'geckodriver'),
+            **dict.fromkeys(['safari'], 'safari')
+            }
+
+        for case, expected in input_expected.items():
+            actual = wd.WebDriver._identify_browser(self,case)
+            self.assertEqual(expected, actual)
+
+    def test_identify_os(self):
+        with self.assertRaises(NotImplementedError):
+            wd.WebDriver._identify_os(self,os='TampleOS')
+        
+        input_expected = {
+            **dict.fromkeys(['Linux'],'linux'), 
+            **dict.fromkeys(['darwin'], 'mac'),
+            **dict.fromkeys(['Windows'], 'win')
+            }
+
+        for case, expected in input_expected.items():
+            actual = wd.WebDriver._identify_os(self,case)
+            self.assertEqual(expected, actual)
+
+    def test_find_executable_path(self):
+        with self.assertRaises(FileNotFoundError):
+            wd.WebDriver._find_executable_path(self, 'NetScape','TampleOS')
+            
+        pathtype = pathlib.PurePosixPath if sys.platform in ('darwin', 'linux') else pathlib.PureWindowsPath
+        input_expected = {
+            **dict.fromkeys([('chromedriver','mac')], pathtype),
+            **dict.fromkeys([('geckodriver','mac')], pathtype),
+            **dict.fromkeys([('safari','mac')], pathtype)
+            }
+
+        for case, expected in input_expected.items():
+            actual = type(wd.WebDriver._find_executable_path(self,*case))
+            self.assertIs(expected, actual)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
